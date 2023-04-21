deb9e3ca645a5e82d6b9ac04cb56ab18f0ad8e3d
stephent2023 <124163684+stephent2023@users.noreply.github.com>
2023-04-21 14:53:27 +0100

    Delete Lab_2.py

--- Lab_2.py
+++
@@ -1,9 +0,0 @@
-# Maka a books dictionary, with 3 authors as keys and multiple books as values.
-#Use an input() asking for author name and print as a string the list of books by that 
-# author. Use the .join() method. 
-
-books_dictionary = {"Albert Camus": ["The Outsider","The Stranger"],"Joe Weaver": ["Devops 101","Stunlocked"]}
-
-author = input("Enter the name of an author: ")
-
-print(",".join(books_dictionary[author]))
