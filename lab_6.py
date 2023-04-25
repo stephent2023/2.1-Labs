fe488483222864f32c372bc88f01172202d0d626
stephent2023 <124163684+stephent2023@users.noreply.github.com>
2023-04-25 13:54:47 +0100

    Update lab_6.py

--- lab_6.py
+++ lab_6.py
@@ -4,12 +4,12 @@ def getIncomeTax(salary):
     if salary > 150000:
         tax = (salary-150000)*0.45
         tax += 115500*0.4
-        tax += 34500*0.2
+        tax += 22650*0.2
     elif salary > 34500:
         tax = (salary-34500)*0.4
-        tax += 34500*0.2
+        tax += 22650*0.2
     elif salary > 0:
-        tax= salary*0.2
+        tax= (salary-11850)*0.2
     else:
         return "error"
     return tax
